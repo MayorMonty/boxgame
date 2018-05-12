f80bf403696a7fd62bd259270b7a43fe3effed9d
MayorMonty <artificalchamber@gmail.com>
2018-05-12 01:17:24 -0400

    Loop entities instead of deleting them

--- src/systems/Physics.js
+++ src/systems/Physics.js
@@ -6,22 +6,21 @@ const Physics = new EntitySystem(({ entity, index, canvas }) => {
   entity.position.x += entity.velocity.x * entity.velocity.scaleX;
   entity.position.y += entity.velocity.y * entity.velocity.scaleY;
 
-  if (entity.position.y < 0 || entity.position.y > canvas.height)
-    delete Entity.list[index];
-
-  if (entity.position.x < 0 || entity.position.x > canvas.width)
-    delete Entity.list[index];
+  if (entity.position.x < 0) entity.position.x = canvas.width;
+  if (entity.position.x > canvas.width) entity.position.x = 0;
 
   // Reduce the velocity
   entity.velocity.x =
     Math.sign(entity.velocity.x) *
     (Math.abs(entity.velocity.x) - entity.velocity.frictionX);
 
-  if (Math.abs(entity.velocity.x) < 1) entity.velocity.x = 0;
+  if (Math.abs(entity.velocity.x) < entity.velocity.frictionX / 2)
+    entity.velocity.x = 0;
 
   entity.velocity.y =
     Math.sign(entity.velocity.y) *
     (Math.abs(entity.velocity.y) - entity.velocity.frictionY);
 
-  if (Math.abs(entity.velocity.y) < 1) entity.velocity.y = 0;
+  if (Math.abs(entity.velocity.y) < entity.velocity.frictionY / 2)
+    entity.velocity.y = 0;
 });
