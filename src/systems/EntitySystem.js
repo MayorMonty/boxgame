c81c7d3682f6c07ae9c636b4d12d5f80081040e6
MayorMonty <artificalchamber@gmail.com>
2018-05-11 23:57:45 -0400

    Pass indexes and EntityList to EntitySystems

--- src/systems/EntitySystem.js
+++ src/systems/EntitySystem.js
@@ -3,6 +3,10 @@ import { Entity } from "../entities";
 
 export class EntitySystem extends System {
   constructor(action) {
-    super(passed => Entity.list.map(entity => action({ entity, ...passed })));
+    super(passed =>
+      Entity.list.map((entity, index, list) =>
+        action({ entity, index, list, ...passed })
+      )
+    );
   }
 }
