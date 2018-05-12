88cbd975306af8c294e41a80d34255eeec5b4463
MayorMonty <artificalchamber@gmail.com>
2018-05-11 23:58:48 -0400

    Delete entity if it ever leaves bounds

--- src/systems/Physics.js
+++ src/systems/Physics.js
@@ -1,9 +1,16 @@
 import { EntitySystem } from "./EntitySystem";
+import { Entity } from "../entities";
 
-const Physics = new EntitySystem(({ entity }) => {
+const Physics = new EntitySystem(({ entity, index, canvas }) => {
   // Apply the velocity
   entity.position.x += entity.velocity.x * entity.velocity.scaleX;
   entity.position.y += entity.velocity.y * entity.velocity.scaleY;
+
+  if (entity.position.y < 0 || entity.position.y > canvas.height)
+    delete Entity.list[index];
+
+  if (entity.position.x < 0 || entity.position.x > canvas.width)
+    delete Entity.list[index];
 
   // Reduce the velocity
   entity.velocity.x =
