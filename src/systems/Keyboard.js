cbf01a78aaf77d0539b883a132f879481c3bf48b
MayorMonty <artificalchamber@gmail.com>
2018-09-09 03:36:26 -0400

    TouchSupport

--- src/systems/Keyboard.js
+++ src/systems/Keyboard.js
@@ -1,8 +1,10 @@
 import { EventEmitter } from "events";
 import { EntitySystem } from "./EntitySystem";
+import { Player } from "../entities";
 
 const state = new Set();
 const events = new EventEmitter();
+const touches = [];
 
 document.addEventListener("keydown", e => {
   state.add(e.code);
@@ -12,9 +14,23 @@ document.addEventListener("keydown", e => {
 document.addEventListener("keyup", e => state.delete(e.code));
 window.addEventListener("blur", () => state.clear());
 
+document.addEventListener("touchstart", e => {
+  touches.splice(0, touches.length);
+  touches.push(...e.changedTouches);
+});
+document.addEventListener("touchend", e => {
+  touches.splice(0, touches.length);
+  Keyboard.state.delete("ArrowUp");
+});
+document.addEventListener("touchmove", e => {
+  touches.splice(0, touches.length);
+  touches.push(...e.changedTouches);
+});
+
 const Keyboard = {
   state,
-  events
+  events,
+  touches
 };
 
 export { Keyboard };
